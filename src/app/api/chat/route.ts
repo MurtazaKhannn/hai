094eec8bbdeb2096197e07ef23fc4a3bcebbb673
MurtazaKhannn <murtazakhan1910@gmail.com>
2025-03-14 14:58:01 +0530

    again

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
