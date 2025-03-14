c14194dbb097f558395572f9715e2fd978000e09
MurtazaKhannn <murtazakhan1910@gmail.com>
2025-03-14 14:52:41 +0530

    apos prblm push

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -4,7 +4,7 @@ export function Appbar() {
   return (
     <div>
       <div className="flex justify-between items-center px-10 py-4  text-white">
-        <div className="font-vina text-xl text-black">Disclaimer : Easy there, let's chat—without getting as wild as the app name!</div>
+        <div className="font-vina text-xl text-black">Disclaimer : Easy there, let&apos;s chat—without getting as wild as the app name!</div>
       </div>
     </div>
   );
